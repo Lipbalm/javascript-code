0f23f47cd47cd8ff7a68673c1336ef5ae04a6dab
Jun <wnsrl56@gmail.com>
2019-07-29 18:13:41 +0900

    remove some code for review

--- Table/table.js
+++ Table/table.js
@@ -1,11 +1,10 @@
 const init = () => {
-    const tableId = createHtmlObject("table", "div"); //생성된 table.Id 
+    const tableId = createHtmlObject('table', 'div'); //생성된 table.Id
     let table = document.getElementById(tableId);
-    setTableByObject(table, getData())
-    table.classList.add(".table")
-
+    setTableByObject(table, getData());
+    table.classList.add('.table');
     return true;
-}
+};
 
 const createHtmlObject = (element, containerdiv) => {
     let div = document.getElementById(containerdiv);
@@ -13,40 +12,41 @@ const createHtmlObject = (element, containerdiv) => {
     createHtmlElement.id = `${containerdiv}_${createHtmlElement}`;
     div.appendChild(createHtmlElement);
     return createHtmlElement.id;
-}
+};
 
-const getTagName = (name, innerText) => `<${name}>${innerText}</${name}>`
-const getTotalPrice = (q, p) => q * p
-//Number(input).toLocaleString('지역') 인자로 들어온 지역 값에 따른 숫자 표기 방식을 적용하여, 문자열로 반환하는 역할 date 표기 시에도 사용 가능.
-const getCurrencyUnit = (input) => input === "" ? "" : Number(input).toLocaleString('en')
+const getTagName = (name, innerText) => `<${name}>${innerText}</${name}>`;
+const getTotalPrice = (q, p) => q * p;
+const getCurrencyUnit = input =>
+    input === '' ? '' : Number(input).toLocaleString('en');
 
 const setTableByObject = (table, ary = []) => {
     let combineBody = [];
     let combineHead = [];
     let headIdx = 0;
 
-    //head 만드는 부분...
-    for (let [key, value] of Object.entries(ary[0].product)) {
-        let th = ""
-        combineHead[headIdx] = getTagName("th", key)
-        headIdx +=1
+    for (let [key] of Object.entries(ary[0].product)) {
+        combineHead[headIdx] = getTagName('th', key);
+        headIdx += 1;
     }
-    combineHead[headIdx + 1] = getTagName("th", "Total Price");
 
-    //body 만드는 부분...
+    combineHead[headIdx + 1] = getTagName('th', 'Total Price');
+
     ary.forEach((v, i) => {
-        const { name, price, quntity } = v.product
-        let td = ""
-        td = `${td}${getTagName("td", name)}`;
-        td = `${td}${getTagName("td", quntity)}`;
-        td = `${td}${getTagName("td", getCurrencyUnit(price))}`;
-        td = `${td}${getTagName("td", getCurrencyUnit(getTotalPrice(quntity, price)))}`
-
-        combineBody[i] = getTagName("tr", td);
+        const { name, price, quntity } = v.product;
+        let res = '';
+        res = [
+            getTagName('td', name),
+            getTagName('td', quntity),
+            getTagName('td', getCurrencyUnit(price)),
+            getTagName('td', getCurrencyUnit(getTotalPrice(quntity, price)))
+        ].join('');
+        combineBody[i] = getTagName('tr', res);
     });
 
-    table.innerHTML = getTagName("thead", combineHead.reduce((av, cv) => av + cv )) + getTagName("tbody", combineBody.reduce((av, cv) => av + cv ));
-}
+    table.innerHTML =
+        getTagName('thead', combineHead.reduce((av, cv) => av + cv)) +
+        getTagName('tbody', combineBody.reduce((av, cv) => av + cv));
+};
 
 let getData = () => {
     return [
@@ -100,6 +100,6 @@ let getData = () => {
             }
         }
     ];
-}
+};
 
 init();
