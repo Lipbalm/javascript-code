53ef654b7b457773679b0383955ccc39576dc509
Jeonghun Kim <jeonghunkim@Jeonghunui-MacBookPro.local>
2019-08-04 15:53:38 +0900

    using documentFragment

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,22 +1,46 @@
-const getTagNm = (text, name) => `<${name}>${text}</${name}>` 
-const div = document.querySelector("div");
-const createAddList = (item) => {
-    let res = '';
-    res = [
-         getTagNm(item,"li"),
-         getTagNm("Complete","button"),
-         getTagNm("Delete", "button")
-    ].join('');
+const getTagNm = (text, name) => `<${name}>${text}</${name}>`;
+const htmlBody = document.querySelector("body");
 
-    return getTagNm(res,"div");
-}
+const createAddList = (item) => {
+    let innerDiv = document.createElement("div");
+    let textli = document.createElement("li");
+    let completeButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+    let checkBox  = createInput("checkBox",`${item}_checkBox`);
+
+    innerDiv.classList.add("item_div");
+    textli.innerText = item;
+    completeButton.innerText = "Complete";
+    deleteButton.innerText = "Delete";
+
+    innerDiv.appendChild(checkBox);
+    innerDiv.appendChild(textli);
+    innerDiv.appendChild(completeButton);
+    innerDiv.appendChild(deleteButton);
+
+    return innerDiv;
+};
+const createInput = (type, id) =>{
+    let res = document.createElement("input");
+    res.id = id;
+    res.setAttribute("type",type);
+    return res;
+};
+
+const submitTodoItem = (event) =>{
+    console.log(event.prototype);
+};
 
 const init = () =>{
-    let itemlist = [];
-    for(var i = 1;  i <=10; i++){
-        itemlist.push(createAddList(`item_${i}`));
-    }
-    div.innerHTML = itemlist.reduce((av, cv) => av + cv);
-}
+    let docfrag = document.createDocumentFragment();
+    let inputText = createInput("input", "inputBox");
+    docfrag.appendChild(inputText);
+    inputText.addEventListener("submit",submitTodoItem);
+    
+    // for(var i = 1;  i <=10; i++){
+    //     docfrag.appendChild(createAddList(`item_${i}`));
+    // }
+    // htmlBody.appendChild(docfrag);
+};
 
 init();
