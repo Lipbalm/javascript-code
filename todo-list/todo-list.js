5fa88994879e064cfe98550620aefd91c2386ff8
Jeonghun Kim <jeonghunkim@Jeonghunui-MacBookPro.local>
2019-08-07 01:00:36 +0900

    use localStorage And change html structure

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,41 +1,70 @@
-const getTagNm = (text, name) => `<${name}>${text}</${name}>`;
 let totalItemList = [];
-
+const getTagNm = (text, name) => `<${name}>${text}</${name}>`;
+const getObj = (k, v) => {return {key : k, value : v}};
+const getHtmlObjKey = (obj) => obj.id.split("_")[1];
+const getChildElementById = (parent, tofind) =>{
+    parent.children.forEach( element => {
+       if(v.id === tofind){
+           return v;
+       } 
+    });
+};
+const addData = (list, k, v) => {
+    localStorage.setItem(k,v);
+    return list.push(getObj(k,v));
+};
 const getMaxKey = (list = []) => {
-    let res = 1;
-    for(let i = 0;  i <= list.length - 1; i++){
-        if(list[i].key >= res){
-            res += 1;
+    let res = 0;
+    list.forEach(v => {
+        if(v.key > res){
+            res = v.key;
         }
-    }
+    })
     return res;
 };
-
-const addItemData = (list = [], value) => {
-    let data = { key: getMaxKey(list), value: value };
-    list.push(data);
+const getAllStorage = (list = []) => {
+    const keys = Object.keys(localStorage);
+    keys.forEach(k => {
+        list.push(getObj(Number(k) ,localStorage.getItem(k))); //type string -> number;
+    });
+    return list;
 };
-
-const createAddList = (item) => {
-    let innerDiv = document.createElement("div");
+const createItem = (key,item) => {
+    let innerDiv = createDiv(`div_${key}`, "item_div");
     let itemli = document.createElement("li");
-    let completeButton = document.createElement("button");
+    let finishButton = document.createElement("button");
     let deleteButton = document.createElement("button");
-    let checkBox = createInput("checkBox", `${item}_checkBox`);
 
-    innerDiv.classList.add("item_div");
+    //set Id
+    let checkBox = createInputBox("checkBox", `checkBox_${key}`);
+    itemli.id = `li_${key}`;
+    finishButton.id = `finishBtn_${key}`;
+    deleteButton.id = `deleteBtn_${key}`;
+
     itemli.innerText = item;
-    completeButton.innerText = "Complete";
+    finishButton.innerText = "Finish";
     deleteButton.innerText = "Delete";
+
+    //add button event
+    finishButton.addEventListener("click",finishTodo);
+    deleteButton.addEventListener("click",deleteTodo);
 
     innerDiv.appendChild(checkBox);
     innerDiv.appendChild(itemli);
-    innerDiv.appendChild(completeButton);
+    innerDiv.appendChild(finishButton);
     innerDiv.appendChild(deleteButton);
 
     return innerDiv;
 };
-const createInput = (type, id) => {
+
+const createDiv = (id, attCls) => {
+    let div = document.createElement("div");
+    div.id = id;
+    div.classList.add(attCls);
+    return div
+};
+
+const createInputBox = (type, id) => {
     let res = document.createElement("input");
     res.id = id;
     res.setAttribute("type", type);
@@ -47,15 +76,18 @@ const addItem = (event) => {
         let inputBox = document.getElementById("inputBox");
 
         if (inputBox.value !== "") {
-            const currentItem = createAddList(inputBox.value);
-            addHtmlObject(currentItem);
-            addItemData(totalItemList, inputBox.value);
+            let mainDiv = document.getElementById("main_div");
+            const maxKey = getMaxKey(totalItemList) + 1;//new SeqNo
+            const currentItem = createItem(maxKey, inputBox.value);
+            //appendHtml(currentItem);
+            mainDiv.insertBefore(currentItem, mainDiv.firstChild);
+            addData(totalItemList, maxKey, inputBox.value);
             inputBox.value = "";
         }
     }
 };
 
-const addHtmlObject = (htmlObj) => {
+const appendHtml = (htmlObj) => {
     const htmlBody = document.querySelector("body");
     let docfrag = document.createDocumentFragment();
     docfrag.appendChild(htmlObj);
@@ -63,13 +95,35 @@ const addHtmlObject = (htmlObj) => {
 };
 
 const init = () => {
-    let inputBox = createInput("input", "inputBox");
+    let inputBox = createInputBox("input", "inputBox");
+    //let docfrag = document.createDocumentFragment();
+    let mainDiv = createDiv("main_div", "main_div");
+
+    //enter  
     inputBox.addEventListener("keypress", addItem);
-    addHtmlObject(inputBox);
-    //add locaStorage data...
-    // for(var i = 1;  i <=10; i++){
-    //     docfrag.appendChild(createAddList(`item_${i}`));
-    // }
+    appendHtml(inputBox);
+ 
+    //add locaStorage...
+    totalItemList = getAllStorage(totalItemList);
+    totalItemList.forEach(v => {
+        mainDiv.appendChild(createItem(v.key, v.value));
+    });
+    appendHtml(mainDiv);
+};
+
+//finish click
+const finishTodo = (event) => {
+    const itemKey = getHtmlObjKey(event.srcElement);
+    const mainDiv = document.getElementById("main_div");
+    const repli =  getChildElementById(getChildElementById(mainDiv, `div_${itemKey}`),`li_${itemKey}`);
+    
+    //insert <del> to repli...
+    
+};   
+
+//delete click
+const deleteTodo = (event) =>{
+
 };
 
 init();
