ed5d8f57e0b5cca4d2742b1b6dbfc613f7cefbb6
Lipbalm <dnstn1118@gmail.com>
2019-09-06 08:22:16 +0900

    add regexp

--- new-todo-list/new-todo-list.js
+++ new-todo-list/new-todo-list.js
@@ -1,61 +1,81 @@
-// const manageElement = () => {
-//     let elements = [];
+let items = [];
+let elements = [];
 
-//     const
-// }
+const regexp = {
+    input: regexp.setRegexp(pattern)
+    ,setRegexp: (pattern) => new RegExp(pattern)
+    ,isMatch: (value, pattern = "") => {
+        let temp;
+        if(pattern === ""){
+            temp = regexp.input;
+        }else{
+           temp = regexp.setRegexp(pattern);
+        }
+    }
+}
 
-function manageItems(){
-    let items = [];
+const manageElement = {
+    getTags: (tag) => `<${tag}></${tag}>`
+    , addAttribute: (attributeName, value) => {
 
-    function getItems() {
+    }
+    , removeAttribute: (attributeName, value) => {
+
+    }
+    , addHtml: (obj) => {
+
+    }
+}
+
+const manageItems = {
+    getItems: (items) => {
         const keys = Object.keys(localStorage);
-
+        items = [];
         keys.forEach(k => {
             items.push(JSON.parse(localStorage.getItem(k)));
         });
 
         return items;
     }
-
-    const addItem = (value, check, date) => {
-        let item = {
+    , addItem: (items, value, check, date) => {
+        let todo = {
             item: {
-                key: manageItems.getMaxKey(),
+                key: manageItems.getMaxKey(items),
                 value: value,
                 check: check,
                 date: date
             }
         };
-
-        items.push(item);
-        localStorage.setItem(JSON.stringify(item));
+        localStorage.setItem(todo.item.key, JSON.stringify(todo));
+        return todo;
     }
-
-    const removeItem = (key) => {
+    , removeItem: (items, key) => {
+        let tmpItems = [];
         items.forEach((v, i, a) => {
-            if (v.item.key === String(key)) {
-                items = a.filter((v, j) => i !== j);
+            if (v.item.key === key) {
+                localStorage.removeItem(String(key));
+                tmpItems = a.filter((v, j) => i !== j);
             }
         });
+        return tmpItems;
     }
-
-    const getMaxKey = () => {
+    , getMaxKey: (items) => {
         let maxKey = 0;
         items.forEach((v) => {
             maxKey = v.item.key > maxKey ? v.item.key : maxKey;
         });
 
-        return maxKey;
+        return maxKey + 1;
     }
-
-    const setCheckItem = (key) => {
+    , setCheckItem: (items, key) => {
         items.forEach((v) => {
             if (v.item.key === String(key)) {
-                if (v.item.check === "Y") {
-                    v.item.check === "N";
+                if (v.item.check === true) {
+                    v.item.check === false;
                 } else {
-                    v.item.check === "Y";
+                    v.item.check === true;
                 }
+                return items;
             }
         });
     }
