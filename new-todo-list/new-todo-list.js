3d0889356d8cb52844892487121a07015d9b1874
Lipbalm <dnstn1118@gmail.com>
2019-09-14 00:25:57 +0900

    update todo-list

--- new-todo-list/new-todo-list.js
+++ new-todo-list/new-todo-list.js
@@ -2,78 +2,65 @@ let items = [];
 let elements = [];
 
 const regexp = {
-    input: regexp.setRegexp(pattern)
-    ,setRegexp: (pattern) => new RegExp(pattern)
-    ,isMatch: (value, pattern = "") => {
-        let temp;
-        if(pattern === ""){
-            temp = regexp.input;
-        }else{
-           temp = regexp.setRegexp(pattern);
+    setPattern: (pattern) => new RegExp(pattern)
+    , isMatch: (pattern, value) => {
+        return regexp.setPattern(pattern).test(value);
+    }
+    , Replace: (value, pattern, replacement) => {
+        return regexp.setPattern(pattern).replace(value, replacement);
+    }
+    , MatchCollection: (pattern, value) => {
+        return regexp.setPattern(pattern).exec(value);
+    }
+}
+const mngElem = {
+    getTags: (tag) => `<${tag}></${tag}>`
+    , getAttrRegex: (param) => `${param} = \"[a-zA-Z0-9_ ]*\"`
+    , addAttr: (item, attrName, value) => {
+        let matValue;
+        if (regexp.isMatch(attrName, item)) {
+            matValue = regexp.MatchCollection(getAttrRegex(attrName), item)[0];
+            return regexp.Replace(item, matValue, `${matValue} ${value}`);
+        } else {
+            return `${item} ${attrName} = "${value}"`
         }
     }
-}
-
-const manageElement = {
-    getTags: (tag) => `<${tag}></${tag}>`
-    , addAttribute: (attributeName, value) => {
-
+    , removeAttr: (item, attrName, value) => {
+        let matValue;
+        matValue = regexp.MatchCollection(getAttrRegex(attrName), item)[0];
+        return regexp.replace(item, matValue, regexp.replace(matValue, value, ''));
     }
-    , removeAttribute: (attributeName, value) => {
-
-    }
-    , addHtml: (obj) => {
+    , addHtml: (...list) => {
 
     }
 }
 
-const manageItems = {
-    getItems: (items) => {
+const mngItems = {
+    getItems: () => {
         const keys = Object.keys(localStorage);
         items = [];
         keys.forEach(k => {
             items.push(JSON.parse(localStorage.getItem(k)));
         });
-
         return items;
     }
-    , addItem: (items, value, check, date) => {
-        let todo = {
-            item: {
-                key: manageItems.getMaxKey(items),
-                value: value,
-                check: check,
-                date: date
-            }
-        };
-        localStorage.setItem(todo.item.key, JSON.stringify(todo));
-        return todo;
-    }
-    , removeItem: (items, key) => {
-        let tmpItems = [];
-        items.forEach((v, i, a) => {
-            if (v.item.key === key) {
-                localStorage.removeItem(String(key));
-                tmpItems = a.filter((v, j) => i !== j);
-            }
-        });
-        return tmpItems;
-    }
+    //, addItem: (items, key, value, check, date) => items.push({ key: key, value: value, check: check, date: date })
+    //, removeItem: (items, key) => items.filter((v) => v.key !== key)
     , getMaxKey: (items) => {
         let maxKey = 0;
         items.forEach((v) => {
-            maxKey = v.item.key > maxKey ? v.item.key : maxKey;
+            maxKey = v.key > maxKey ? v.key : maxKey;
         });
-
         return maxKey + 1;
     }
     , setCheckItem: (items, key) => {
         items.forEach((v) => {
-            if (v.item.key === String(key)) {
-                if (v.item.check === true) {
-                    v.item.check === false;
+            const { check, key } = v;
+            if (v.key === String(key)) {
+                if (v.check === true) {
+                    v.check === false;
                 } else {
-                    v.item.check === true;
+                    v.check === true;
                 }
                 return items;
             }
@@ -81,7 +68,26 @@ const manageItems = {
     }
 }
 
-const init = () => {
+const initItem = (elemNm, id, cls) => {
+    let tmpElem;
+    tmpElem = mngElem.getTags(elemNm);
+    tmpElem = mngElem.addAttr(tmpElem, 'id', id);
+    tmpElem = mngElem.addAttr(tmpElem, 'class', cls);
 
+    return tmpElem;
+}
+const init = () => {
+    items = mngItems.getItems();
+
+    items.forEach((v) => {
+        const { key, value, check, date } = v;
+        let div = initItem('div', `item_div_${key}`, 'item_div')
+        let li = initItem('li', `item_li_${key}`, 'item_li')
+        let btnCom = initItem('button', `item_btnCom_${key}`, 'fas fa-check fa-2x')
+        let btnDel =initItem('button', `item_btnDel_${key}`, 'fas fa-minus fa-2x')
+
+        
+
+    });
 }
 init();
